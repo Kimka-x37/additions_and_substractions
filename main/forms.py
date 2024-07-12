5d8142552332a246e2822f9e41140b61023f9257
Kim <kimka230510@gmail.com>
2024-07-12 20:12:09 +0200

    register_bug_fix

--- main/forms.py
+++ main/forms.py
@@ -1,25 +1,30 @@
 from django.contrib.auth.models import User
-from django.forms import ModelForm, TextInput, Form
+from django.forms import TextInput, Form, PasswordInput
 from django.forms import CharField
+from django.contrib.auth.forms import UserCreationForm
 
-class RegisterForm(ModelForm):
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ["username", "password1", "password2"]
         widgets = {
             'username': TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Введите ник'
             }),
-            'password': TextInput(attrs={
+            'password1': PasswordInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Введите пароль'
+            }),
+            'password2': PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите ник'
             })
         }
 class LoginForm(Form):
     username = CharField(
         max_length=150,
-        required=True, 
+        required=True,
         widget=TextInput(attrs={
                     'class': 'form-control',
                     'placeholder': 'Введите ник'
@@ -27,7 +32,7 @@ class LoginForm(Form):
     )
     password = CharField(
         max_length=128,
-        required=True, 
+        required=True,
         widget=TextInput(attrs={
                     'class': 'form-control',
                     'placeholder': 'Введите пароль'
